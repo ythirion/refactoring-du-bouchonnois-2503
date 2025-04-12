bd680360ad77c0d474bacbde2f60c9ef46cd1052
Cédric Hulin <hulin.cedric@gmail.com>
2025-04-12 17:26:20 +0200

    ✅ [TCR - PASSED] tests passing
    changed-lines:
        src: 0
        test: 86
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- Bouchonnois.Tests/UseCases/ConsulterStatus.cs
+++ Bouchonnois.Tests/UseCases/ConsulterStatus.cs
@@ -33,54 +33,56 @@ public class ConsulterStatus : UseCaseTest
             UnePartieDeChasse()
                 .AvecEvenements(
                     // @formatter:off
-                    new (new (2024, 04, 25, 09, 00, 12), "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"),
-                    new (new (2024, 04, 25, 09, 10, 00), "Dédé tire"),
-                    new (new DateTime(2024, 04, 25, 09, 40, 00), "Robert tire sur une galinette"),
-                    new (new DateTime(2024, 04, 25, 10, 00, 00), "Petit apéro"),
-                    new (new DateTime(2024, 04, 25, 11, 00, 00), "Reprise de la chasse"),
-                    new (new DateTime(2024, 04, 25, 11, 02, 00), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 11, 03, 00), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 11, 04, 00), "Dédé tire sur une galinette"),
-                    new (new DateTime(2024, 04, 25, 11, 30, 00), "Robert tire sur une galinette"),
-                    new (new DateTime(2024, 04, 25, 11, 40, 00), "Petit apéro"),
-                    new (new DateTime(2024, 04, 25, 14, 30, 00), "Reprise de la chasse"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 00), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 01), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 02), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 03), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 04), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 05), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 06), "Bernard tire"),
-                    new (new DateTime(2024, 04, 25, 14, 41, 07), "Bernard tire -> T'as plus de balles mon vieux, chasse à la main"),
-                    new (new DateTime(2024, 04, 25, 15, 00, 00), "Robert tire sur une galinette"),
-                    new (new DateTime(2024, 04, 25, 15, 30, 00), "La partie de chasse est terminée, vainqueur :  Robert - 3 galinettes")));
+                    new Event(new DateTime(2024, 04, 25, 09, 00, 12), "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"),
+                    new Event(new DateTime(2024, 04, 25, 09, 10, 00), "Dédé tire"),
+                    new Event(new DateTime(2024, 04, 25, 09, 40, 00), "Robert tire sur une galinette"),
+                    new Event(new DateTime(2024, 04, 25, 10, 00, 00), "Petit apéro"),
+                    new Event(new DateTime(2024, 04, 25, 11, 00, 00), "Reprise de la chasse"),
+                    new Event(new DateTime(2024, 04, 25, 11, 02, 00), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 11, 03, 00), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 11, 04, 00), "Dédé tire sur une galinette"),
+                    new Event(new DateTime(2024, 04, 25, 11, 30, 00), "Robert tire sur une galinette"),
+                    new Event(new DateTime(2024, 04, 25, 11, 40, 00), "Petit apéro"),
+                    new Event(new DateTime(2024, 04, 25, 14, 30, 00), "Reprise de la chasse"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 00), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 01), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 02), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 03), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 04), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 05), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 06), "Bernard tire"),
+                    new Event(new DateTime(2024, 04, 25, 14, 41, 07), "Bernard tire -> T'as plus de balles mon vieux, chasse à la main"),
+                    new Event(new DateTime(2024, 04, 25, 15, 00, 00), "Robert tire sur une galinette"),
+                    new Event(new DateTime(2024, 04, 25, 15, 30, 00), "La partie de chasse est terminée, vainqueur :  Robert - 3 galinettes")));
         // @formatter:on
 
         var status = Service.ConsulterStatus(id);
 
         status.Should()
             .BeEquivalentTo(
-                @"15:30 - La partie de chasse est terminée, vainqueur :  Robert - 3 galinettes
-15:00 - Robert tire sur une galinette
-14:41 - Bernard tire -> T'as plus de balles mon vieux, chasse à la main
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:41 - Bernard tire
-14:30 - Reprise de la chasse
-11:40 - Petit apéro
-11:30 - Robert tire sur une galinette
-11:04 - Dédé tire sur une galinette
-11:03 - Bernard tire
-11:02 - Bernard tire
-11:00 - Reprise de la chasse
-10:00 - Petit apéro
-09:40 - Robert tire sur une galinette
-09:10 - Dédé tire
-09:00 - La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)");
+                """
+                15:30 - La partie de chasse est terminée, vainqueur :  Robert - 3 galinettes
+                15:00 - Robert tire sur une galinette
+                14:41 - Bernard tire -> T'as plus de balles mon vieux, chasse à la main
+                14:41 - Bernard tire
+                14:41 - Bernard tire
+                14:41 - Bernard tire
+                14:41 - Bernard tire
+                14:41 - Bernard tire
+                14:41 - Bernard tire
+                14:41 - Bernard tire
+                14:30 - Reprise de la chasse
+                11:40 - Petit apéro
+                11:30 - Robert tire sur une galinette
+                11:04 - Dédé tire sur une galinette
+                11:03 - Bernard tire
+                11:02 - Bernard tire
+                11:00 - Reprise de la chasse
+                10:00 - Petit apéro
+                09:40 - Robert tire sur une galinette
+                09:10 - Dédé tire
+                09:00 - La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)
+                """);
     }
 
     [Fact]
