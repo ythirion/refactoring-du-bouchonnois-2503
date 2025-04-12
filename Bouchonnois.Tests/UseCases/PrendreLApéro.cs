d04c759894a4b398dd7d64ce813e2f7946c0f76b
Cédric Hulin <hulin.cedric@gmail.com>
2025-04-12 17:00:18 +0200

    ✅ [TCR - PASSED] tests passing
    changed-lines:
        src: 0
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- Bouchonnois.Tests/UseCases/PrendreLApéro.cs
+++ Bouchonnois.Tests/UseCases/PrendreLApéro.cs
@@ -1,7 +1,4 @@
-using Bouchonnois.Domain;
-using Bouchonnois.Service;
 using Bouchonnois.Service.Exceptions;
-using Bouchonnois.Tests.Doubles;
 using Bouchonnois.Tests.Verifications;
 using static Bouchonnois.Tests.Builders.PartieDeChasseBuilder;
 
@@ -27,11 +24,11 @@ public class PrendreLApéro : UseCaseTest
     public void EchoueCarPartieNexistePas()
     {
         var id = UnePartieDeChasseInexistante();
-       
+
         var apéroQuandPartieExistePas = () => Service.PrendreLapéro(id);
 
         apéroQuandPartieExistePas.Should().Throw<LaPartieDeChasseNexistePas>();
-       
+
         Repository.SavedPartieDeChasse().Should().BeNull();
     }
 
@@ -39,37 +36,23 @@ public class PrendreLApéro : UseCaseTest
     public void EchoueSiLesChasseursSontDéjaEnApero()
     {
         var id = UnePartieDeChasseExistante(UnePartieDeChasse().ALApéro());
-        
+
         var prendreLApéroQuandOnPrendDéjàLapéro = () => Service.PrendreLapéro(id);
 
         prendreLApéroQuandOnPrendDéjàLapéro.Should().Throw<OnEstDéjàEnTrainDePrendreLapéro>();
-        
+
         Repository.SavedPartieDeChasse().Should().BeNull();
     }
 
     [Fact]
     public void EchoueSiLaPartieDeChasseEstTerminée()
     {
-        var id = Guid.NewGuid();
-        var repository = new PartieDeChasseRepositoryForTests();
+        var id = UnePartieDeChasseExistante(UnePartieDeChasse().Terminée());
 
-        repository.Add(
-            new PartieDeChasse(
-                id: id,
-                chasseurs: new List<Chasseur>
-                {
-                    new(nom: "Dédé", ballesRestantes: 20),
-                    new(nom: "Bernard", ballesRestantes: 8),
-                    new(nom: "Robert", ballesRestantes: 12)
-                },
-                terrain: new Terrain(nom: "Pitibon sur Sauldre", nbGalinettes: 3),
-                status: PartieStatus.Terminée));
+        var prendreLapéroQuandTerminée = () => Service.PrendreLapéro(id);
 
-        var service = new PartieDeChasseService(repository, () => DateTime.Now);
-        var prendreLapéroQuandTerminée = () => service.PrendreLapéro(id);
+        prendreLapéroQuandTerminée.Should().Throw<OnPrendPasLapéroQuandLaPartieEstTerminée>();
 
-        prendreLapéroQuandTerminée.Should()
-            .Throw<OnPrendPasLapéroQuandLaPartieEstTerminée>();
-        repository.SavedPartieDeChasse().Should().BeNull();
+        Repository.SavedPartieDeChasse().Should().BeNull();
     }
 }
