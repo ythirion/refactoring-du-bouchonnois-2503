7d01160838f309b1a5936fa616d555cea1011361
Cédric Hulin <hulin.cedric@gmail.com>
2025-04-12 17:18:47 +0200

    ✅ [TCR - PASSED] tests passing
    changed-lines:
        src: 0
        test: 17
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- Bouchonnois.Tests/UseCases/ConsulterStatus.cs
+++ Bouchonnois.Tests/UseCases/ConsulterStatus.cs
@@ -11,17 +11,16 @@ public class ConsulterStatus : UseCaseTest
     [Fact]
     public void QuandLaPartieVientDeDémarrer()
     {
-        var partieDeChasse = UnePartieDeChasse()
-            .EnCours()
-            .AvecEvenements(
-                new Event(
-                    new DateTime(2024, 4, 25, 9, 0, 12),
-                    "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)"))
-            .Build();
+      var id=  UnePartieDeChasseExistante(
+            UnePartieDeChasse()
+                .EnCours()
+                .AvecEvenements(
+                    new Event(
+                        new DateTime(2024, 4, 25, 9, 0, 12),
+                        "La partie de chasse commence à Pitibon sur Sauldre avec Dédé (20 balles), Bernard (8 balles), Robert (12 balles)")));
 
-        Repository.Add(partieDeChasse);
 
-        var status = Service.ConsulterStatus(partieDeChasse.Id);
+        var status = Service.ConsulterStatus(id);
 
         status.Should()
             .Be(
