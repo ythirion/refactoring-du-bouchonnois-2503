d34aa418ce55845e4c34fd4c630e7077d238fb3c
Imen Bouhamed <imen.bouhamed@sofrecom.com>
2025-04-16 20:30:45 +0100

    simplify assertions by using PartieDeChasse builder in PrendreLApéro use case

--- Bouchonnois.Tests/UseCases/PrendreLApéro.cs
+++ Bouchonnois.Tests/UseCases/PrendreLApéro.cs
@@ -12,38 +12,32 @@ public class PrendreLApéro
     public void QuandLaPartieEstEnCours()
     {
         var repository = new PartieDeChasseRepositoryForTests();
-
+        var dédé = new ChasseurBuilder().Nommé("Dédé").AyantDesBallesRestantes(20).Build();
+        var bernard = new ChasseurBuilder().Nommé("Bernard").AyantDesBallesRestantes(8).Build();
+        var robert = new ChasseurBuilder().Nommé("Robert").AyantDesBallesRestantes(12).Build();
         var partieDeChasse = new PartieDeChasseBuilder()
             .QuiEstEnCours()
-            .AvecDesChasseurs(
-                new List<Chasseur>
-                {
-                    new(nom: "Dédé", ballesRestantes: 20),
-                    new(nom: "Bernard", ballesRestantes: 8),
-                    new(nom: "Robert", ballesRestantes: 12),
-                }
-            )
+            .AvecDesChasseurs(new List<Chasseur> { dédé, bernard, robert })
             .Build();
         repository.Add(partieDeChasse);
+        var now = DateTime.Now;
 
-        var service = new PartieDeChasseService(repository, () => DateTime.Now);
+        var service = new PartieDeChasseService(repository, () => now);
         service.PrendreLapéro(partieDeChasse.Id);
 
         var savedPartieDeChasse = repository.SavedPartieDeChasse();
-        savedPartieDeChasse.Id.Should().Be(partieDeChasse.Id);
-        savedPartieDeChasse.Status.Should().Be(PartieStatus.Apéro);
-        savedPartieDeChasse.Terrain.Nom.Should().Be("Pitibon sur Sauldre");
-        savedPartieDeChasse.Terrain.NbGalinettes.Should().Be(3);
-        savedPartieDeChasse.Chasseurs.Should().HaveCount(3);
-        savedPartieDeChasse.Chasseurs[0].Nom.Should().Be("Dédé");
-        savedPartieDeChasse.Chasseurs[0].BallesRestantes.Should().Be(20);
-        savedPartieDeChasse.Chasseurs[0].NbGalinettes.Should().Be(0);
-        savedPartieDeChasse.Chasseurs[1].Nom.Should().Be("Bernard");
-        savedPartieDeChasse.Chasseurs[1].BallesRestantes.Should().Be(8);
-        savedPartieDeChasse.Chasseurs[1].NbGalinettes.Should().Be(0);
-        savedPartieDeChasse.Chasseurs[2].Nom.Should().Be("Robert");
-        savedPartieDeChasse.Chasseurs[2].BallesRestantes.Should().Be(12);
-        savedPartieDeChasse.Chasseurs[2].NbGalinettes.Should().Be(0);
+
+        savedPartieDeChasse.Should().BeEquivalentTo(new PartieDeChasseBuilder()
+            .AyantLId(partieDeChasse.Id)
+            .QuiEstApero()
+            .AvecDesChasseurs(new List<Chasseur> { dédé, bernard, robert })
+            .AvecSesEvenements(new List<Event>
+            {
+                new(now,
+                    "Petit apéro")
+            })
+            .Build()
+        );
     }
 
     [Fact]
