778520c2713fb1dc4d0a2bce96922fc2b26ba660
Cédric Hulin <hulin.cedric@gmail.com>
2025-04-24 23:23:06 +0200

    refactor: Extract ConsulterStatusUseCase

--- Bouchonnois/Service/PartieDeChasseService.cs
+++ Bouchonnois/Service/PartieDeChasseService.cs
@@ -1,22 +1,21 @@
 using Bouchonnois.Domain;
-using Bouchonnois.Service.Exceptions;
 
 namespace Bouchonnois.Service
 {
     public class PartieDeChasseService
     {
-        private readonly IPartieDeChasseRepository _repository;
         private readonly DemarrerUnePartieDeChasseUseCase _demarrerUnePartieDeChasseUseCase;
         private readonly TirerUseCase _tirerUseCase;
         private readonly PrendreLapéroUseCase _prendreLapéroUseCase;
         private readonly ReprendreLaPartieUseCase _reprendreLaPartieUseCase;
         private readonly TerminerLaPartieUseCase _terminerLaPartieUseCase;
+        private readonly ConsulterStatusUseCase _consulterStatusUseCase;
 
         public PartieDeChasseService(
             IPartieDeChasseRepository repository,
             Func<DateTime> timeProvider)
         {
-            _repository = repository;
+            _consulterStatusUseCase = new ConsulterStatusUseCase(repository);
             _terminerLaPartieUseCase = new TerminerLaPartieUseCase(repository, timeProvider);
             _reprendreLaPartieUseCase = new ReprendreLaPartieUseCase(repository, timeProvider);
             _prendreLapéroUseCase = new PrendreLapéroUseCase(repository, timeProvider);
@@ -35,21 +34,6 @@ namespace Bouchonnois.Service
 
         public string TerminerLaPartie(Guid id) => _terminerLaPartieUseCase.TerminerLaPartie(id);
 
-        public string ConsulterStatus(Guid id)
-        {
-            var partieDeChasse = _repository.GetById(id);
-
-            if (partieDeChasse == null)
-            {
-                throw new LaPartieDeChasseNexistePas();
-            }
-
-            return string.Join(
-                Environment.NewLine,
-                partieDeChasse.Events
-                    .OrderByDescending(@event => @event.Date)
-                    .Select(@event => @event.ToString())
-            );
-        }
+        public string ConsulterStatus(Guid id) => _consulterStatusUseCase.ConsulterStatus(id);
     }
 }
