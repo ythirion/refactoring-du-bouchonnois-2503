7e4a51a07e32e3edc83aa5639263936938d0ecbd
cedric <cedric.meleard@gmail.com>
2025-05-01 18:47:49 +0200

    fix: reduce nesting
    - Prevent shooting during "apéro" phase and add appropriate events.
    - Ensure no shooting is allowed once the game is marked as "terminated".
    - Refactor validations for clearer flow and improved error handling.

--- Bouchonnois/UseCases/Commands/TirerUseCase.cs
+++ Bouchonnois/UseCases/Commands/TirerUseCase.cs
@@ -70,46 +70,44 @@ public class TirerUseCase(IPartieDeChasseRepository repository, Func<DateTime> t
         {
             var partieDeChasse = repository.GetById(id);
 
-            if (partieDeChasse.Status != PartieStatus.Apéro)
-            {
-                if (partieDeChasse.Status != PartieStatus.Terminée)
-                {
-                    var chasseurQuiTire = partieDeChasse.GetChasseurs().FirstOrDefault(c => c.Nom == chasseur);
-                    if (chasseurQuiTire is not null)
-                    {
-                        if (chasseurQuiTire.BallesRestantes == 0)
-                        {
-                            partieDeChasse.Events.Add(new Event(timeProvider(),
-                                $"{chasseur} tire -> T'as plus de balles mon vieux, chasse à la main"));
-                            repository.Save(partieDeChasse);
-
-                            throw new TasPlusDeBallesMonVieuxChasseALaMain();
-                        }
-
-                        partieDeChasse.Events.Add(new Event(timeProvider(), $"{chasseur} tire"));
-                        chasseurQuiTire.BallesRestantes--;
-                    }
-                    else
-                    {
-                        throw new ChasseurInconnu(chasseur);
-                    }
-                }
-                else
-                {
-                    partieDeChasse.Events.Add(new Event(timeProvider(),
-                        $"{chasseur} veut tirer -> On tire pas quand la partie est terminée"));
-                    repository.Save(partieDeChasse);
-
-                    throw new OnTirePasQuandLaPartieEstTerminée();
-                }
-            }
-            else
+            if (partieDeChasse.Status == PartieStatus.Apéro)
             {
                 partieDeChasse.Events.Add(new Event(timeProvider(),
                     $"{chasseur} veut tirer -> On tire pas pendant l'apéro, c'est sacré !!!"));
                 repository.Save(partieDeChasse);
 
                 return UnitResult.Failure(new Error(DomainErrorMessages.OnTirePasPendantLapéroCestSacré));
+            }
+
+            if (partieDeChasse.Status != PartieStatus.Terminée)
+            {
+                var chasseurQuiTire = partieDeChasse.GetChasseurs().FirstOrDefault(c => c.Nom == chasseur);
+                if (chasseurQuiTire is not null)
+                {
+                    if (chasseurQuiTire.BallesRestantes == 0)
+                    {
+                        partieDeChasse.Events.Add(new Event(timeProvider(),
+                            $"{chasseur} tire -> T'as plus de balles mon vieux, chasse à la main"));
+                        repository.Save(partieDeChasse);
+
+                        throw new TasPlusDeBallesMonVieuxChasseALaMain();
+                    }
+
+                    partieDeChasse.Events.Add(new Event(timeProvider(), $"{chasseur} tire"));
+                    chasseurQuiTire.BallesRestantes--;
+                }
+                else
+                {
+                    throw new ChasseurInconnu(chasseur);
+                }
+            }
+            else
+            {
+                partieDeChasse.Events.Add(new Event(timeProvider(),
+                    $"{chasseur} veut tirer -> On tire pas quand la partie est terminée"));
+                repository.Save(partieDeChasse);
+
+                throw new OnTirePasQuandLaPartieEstTerminée();
             }
 
             repository.Save(partieDeChasse);
