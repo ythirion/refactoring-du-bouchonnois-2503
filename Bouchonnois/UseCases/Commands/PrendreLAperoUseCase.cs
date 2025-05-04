e40690642260ecb48d9b579fd86bb29f6dceaa3e
virmaxdam <damien.oria@gmail.com>
2025-05-04 18:27:07 +0200

    refacto: clean code

--- Bouchonnois/UseCases/Commands/PrendreLAperoUseCase.cs
+++ Bouchonnois/UseCases/Commands/PrendreLAperoUseCase.cs
@@ -13,25 +13,13 @@ public class PrendreLAperoUseCase(IPartieDeChasseRepository repository, Func<Dat
     {
         var partieDeChasse = repository.GetById(id);
 
-        if (partieDeChasse == null)
-        {
-            throw new LaPartieDeChasseNexistePas();
-        }
+        if (partieDeChasse == null) throw new LaPartieDeChasseNexistePas();
+        if (partieDeChasse.Status == PartieStatus.Apéro) throw new OnEstDéjàEnTrainDePrendreLapéro();
+        if (partieDeChasse.Status == PartieStatus.Terminée) throw new OnPrendPasLapéroQuandLaPartieEstTerminée();
 
-        if (partieDeChasse.Status == PartieStatus.Apéro)
-        {
-            throw new OnEstDéjàEnTrainDePrendreLapéro();
-        }
-        else if (partieDeChasse.Status == PartieStatus.Terminée)
-        {
-            throw new OnPrendPasLapéroQuandLaPartieEstTerminée();
-        }
-        else
-        {
-            partieDeChasse.Status = PartieStatus.Apéro;
-            partieDeChasse.Events.Add(new Event(timeProvider(), "Petit apéro"));
-            repository.Save(partieDeChasse);
-        }
+        partieDeChasse.Status = PartieStatus.Apéro;
+        partieDeChasse.Events.Add(new Event(timeProvider(), "Petit apéro"));
+        repository.Save(partieDeChasse);
     }
 
     public UnitResult<Error> HandleWithoutException(Guid id)
@@ -46,17 +34,15 @@ public class PrendreLAperoUseCase(IPartieDeChasseRepository repository, Func<Dat
         {
             return UnitResult.Failure(new Error("On est déjà en train de prendre l'apéro"));
         }
-        else if (partieDeChasse.Status == PartieStatus.Terminée)
+
+        if (partieDeChasse.Status == PartieStatus.Terminée)
         {
             return UnitResult.Failure(new Error("On ne prend pas l'apéro quand la partie est terminée"));
         }
-        else
-        {
-            partieDeChasse.Status = PartieStatus.Apéro;
-            partieDeChasse.Events.Add(new Event(timeProvider(), "Petit apéro"));
-            repository.Save(partieDeChasse);
-        }
 
+        partieDeChasse.Status = PartieStatus.Apéro;
+        partieDeChasse.Events.Add(new Event(timeProvider(), "Petit apéro"));
+        repository.Save(partieDeChasse);
 
         return UnitResult.Success<Error>();
     }
